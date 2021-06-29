c2e05602a743b8155a3f8db5060e8bc624ea2f37
Habib Ali Machpud <habib.gunadarma@gmail.com>
2021-06-29 14:46:09 +0700

    Creating datasource

---
+++ bootstrap/src/main/java/io/github/bluething/spring/bootstrap/harcoded/DevelopmentOnlyCustomerService.java
@@ -0,0 +1,22 @@
+package io.github.bluething.spring.bootstrap.harcoded;
+
+import io.github.bluething.spring.bootstrap.BaseCustomerService;
+import io.github.bluething.spring.bootstrap.DataSourceUtils;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+public class DevelopmentOnlyCustomerService extends BaseCustomerService {
+    protected DevelopmentOnlyCustomerService() {
+        super(buildDataSource());
+    }
+
+    //create datasource using an embedded H2
+    //the datasource will pass into super constructor
+    //ideally we don't implement it like this
+    private static DataSource buildDataSource() {
+        DataSource dataSource = new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
+        return DataSourceUtils.initializeDdl(dataSource);
+    }
+}
