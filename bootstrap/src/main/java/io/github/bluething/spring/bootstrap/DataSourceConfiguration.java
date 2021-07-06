fcadd4c4e45c9fd7a8d826f4454fb751e73f6ca3
Habib Ali Machpud <habib.gunadarma@gmail.com>
2021-07-06 10:19:42 +0700

    @Configuration class as a container for other configuration class
    When this class imported, Spring also resolve any nested configuration.

---
+++ bootstrap/src/main/java/io/github/bluething/spring/bootstrap/DataSourceConfiguration.java
@@ -0,0 +1,11 @@
+package io.github.bluething.spring.bootstrap;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSourceConfiguration {
+    @Configuration
+    public static class ProductionConfiguration {
+
+    }
+}
