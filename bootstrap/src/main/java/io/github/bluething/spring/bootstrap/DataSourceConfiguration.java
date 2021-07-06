2ed083a6acdd33aa0438edc249742a931a532208
Habib Ali Machpud <habib.gunadarma@gmail.com>
2021-07-06 10:20:55 +0700

    Mark when this configuration active

--- bootstrap/src/main/java/io/github/bluething/spring/bootstrap/DataSourceConfiguration.java
+++ bootstrap/src/main/java/io/github/bluething/spring/bootstrap/DataSourceConfiguration.java
@@ -1,10 +1,12 @@
 package io.github.bluething.spring.bootstrap;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class DataSourceConfiguration {
     @Configuration
+    @Profile("prod")
     public static class ProductionConfiguration {
 
     }
