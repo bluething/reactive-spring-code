757191e0143e1c4de79e8c538351d378c2b64c29
Habib Ali Machpud <habib.gunadarma@gmail.com>
2021-07-06 13:16:40 +0700

    Bean provider method

--- bootstrap/src/main/java/io/github/bluething/spring/bootstrap/context/Application.java
+++ bootstrap/src/main/java/io/github/bluething/spring/bootstrap/context/Application.java
@@ -1,11 +1,20 @@
 package io.github.bluething.spring.bootstrap.context;
 
 import io.github.bluething.spring.bootstrap.DataSourceConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 @Import(DataSourceConfiguration.class)
 public class Application {
+    @Bean
+    PlatformTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
 
 }
