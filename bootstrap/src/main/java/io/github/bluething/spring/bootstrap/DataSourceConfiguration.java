ee1410cea1ab2c07747200a26b130295da94af88
Habib Ali Machpud <habib.gunadarma@gmail.com>
2021-07-06 10:28:34 +0700

    Inject value from configuration file
    @PropertySource will take any configuration value from a file and adds them to the Environment. We can get those value with @Value.
    Environment is a Spring object that act like a dictionary for configuration value (key-value). We can teach the Environment about new sources for configuration value by contributing an object of type PropertySource to the Environment.

--- bootstrap/src/main/java/io/github/bluething/spring/bootstrap/DataSourceConfiguration.java
+++ bootstrap/src/main/java/io/github/bluething/spring/bootstrap/DataSourceConfiguration.java
@@ -2,11 +2,13 @@ package io.github.bluething.spring.bootstrap;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 public class DataSourceConfiguration {
     @Configuration
     @Profile("prod")
+    @PropertySource("application-prod.properties")
     public static class ProductionConfiguration {
 
     }
