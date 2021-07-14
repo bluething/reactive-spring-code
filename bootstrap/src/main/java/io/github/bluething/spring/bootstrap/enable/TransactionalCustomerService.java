853f16cd79c1ef70591aeec4c6d42dae70f83ee6
bluething <habib.gunadarma@gmail.com>
2021-07-14 11:00:24 +0700

    Annotated with @Transactional
    All public method in this class have transaction demarcation automatically

--- bootstrap/src/main/java/io/github/bluething/spring/bootstrap/enable/TransactionalCustomerService.java
+++ bootstrap/src/main/java/io/github/bluething/spring/bootstrap/enable/TransactionalCustomerService.java
@@ -3,11 +3,13 @@ package io.github.bluething.spring.bootstrap.enable;
 import io.github.bluething.spring.bootstrap.BaseCustomerService;
 import io.github.bluething.spring.bootstrap.Customer;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.sql.DataSource;
 import java.util.Collection;
 
 @Service
+@Transactional
 public class TransactionalCustomerService extends BaseCustomerService {
     protected TransactionalCustomerService(DataSource ds) {
         super(ds);
