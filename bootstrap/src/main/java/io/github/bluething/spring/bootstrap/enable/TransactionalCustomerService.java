71e706bd1a6502c53d6e8b82c22fa49013dc4cec
bluething <habib.gunadarma@gmail.com>
2021-07-14 10:56:51 +0700

    Add CustomerService bean type

---
+++ bootstrap/src/main/java/io/github/bluething/spring/bootstrap/enable/TransactionalCustomerService.java
@@ -0,0 +1,30 @@
+package io.github.bluething.spring.bootstrap.enable;
+
+import io.github.bluething.spring.bootstrap.BaseCustomerService;
+import io.github.bluething.spring.bootstrap.Customer;
+import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
+import java.util.Collection;
+
+@Service
+public class TransactionalCustomerService extends BaseCustomerService {
+    protected TransactionalCustomerService(DataSource ds) {
+        super(ds);
+    }
+
+    @Override
+    public Collection<Customer> save(String... names) {
+        return super.save(names);
+    }
+
+    @Override
+    public Customer findById(long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Collection<Customer> findAll() {
+        return super.findAll();
+    }
+}
