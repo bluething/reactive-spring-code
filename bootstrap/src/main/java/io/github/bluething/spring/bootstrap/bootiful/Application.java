31b6fe4475bf82a9ea9d52ed1b290ec0c5d73aa7
bluething <habib.gunadarma@gmail.com>
2021-08-20 16:37:25 +0700

    Start the spring boot

--- bootstrap/src/main/java/io/github/bluething/spring/bootstrap/bootiful/Application.java
+++ bootstrap/src/main/java/io/github/bluething/spring/bootstrap/bootiful/Application.java
@@ -1,10 +1,12 @@
 package io.github.bluething.spring.bootstrap.bootiful;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         System.setProperty("spring.profiles.active", "dev");
+        SpringApplication.run(Application.class);
     }
 }
