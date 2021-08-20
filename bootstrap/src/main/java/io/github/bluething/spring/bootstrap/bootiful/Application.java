0308c3ca3bc8e6089452c02a0ab9899798d73081
bluething <habib.gunadarma@gmail.com>
2021-08-20 16:36:16 +0700

    Set spring active profile from Java System properties

--- bootstrap/src/main/java/io/github/bluething/spring/bootstrap/bootiful/Application.java
+++ bootstrap/src/main/java/io/github/bluething/spring/bootstrap/bootiful/Application.java
@@ -5,6 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        
+        System.setProperty("spring.profiles.active", "dev");
     }
 }
