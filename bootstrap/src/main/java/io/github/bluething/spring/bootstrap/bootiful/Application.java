4f721c525fcf29de0f2be0118fbc48179213c5f5
bluething <habib.gunadarma@gmail.com>
2021-08-20 16:39:07 +0700

    Bean that active for dev profile

--- bootstrap/src/main/java/io/github/bluething/spring/bootstrap/bootiful/Application.java
+++ bootstrap/src/main/java/io/github/bluething/spring/bootstrap/bootiful/Application.java
@@ -2,11 +2,19 @@ package io.github.bluething.spring.bootstrap.bootiful;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         System.setProperty("spring.profiles.active", "dev");
         SpringApplication.run(Application.class);
+    }
+
+    @Profile("dev")
+    @Component
+    class DemoListener {
+        
     }
 }
