7521ae5ae227c2f772e7373824e2926fabcd7600
bluething <habib.gunadarma@gmail.com>
2021-08-20 16:35:12 +0700

    Add spring boot main
    @SpringBootApplication is a meta-annotation contain @Configuration, @ComponentScan and @EnableAutoconfiguration.
    @EnableAutoconfiguration will inspect META-INF/spring.factories (we can built own), it will make objects to construct an object graph

---
+++ bootstrap/src/main/java/io/github/bluething/spring/bootstrap/bootiful/Application.java
@@ -0,0 +1,10 @@
+package io.github.bluething.spring.bootstrap.bootiful;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        
+    }
+}
