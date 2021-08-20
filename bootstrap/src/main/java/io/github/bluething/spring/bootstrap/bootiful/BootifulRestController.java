4e002cac7244f8415b0483de5e3ee1f41607e321
bluething <habib.gunadarma@gmail.com>
2021-08-20 16:48:07 +0700

    Create the handler
    Spring MVC know how to match incoming Http request to handler method annotation

--- bootstrap/src/main/java/io/github/bluething/spring/bootstrap/bootiful/BootifulRestController.java
+++ bootstrap/src/main/java/io/github/bluething/spring/bootstrap/bootiful/BootifulRestController.java
@@ -1,7 +1,22 @@
 package io.github.bluething.spring.bootstrap.bootiful;
 
+import io.github.bluething.spring.bootstrap.Customer;
+import io.github.bluething.spring.bootstrap.CustomerService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 public class BootifulRestController {
+    private final CustomerService customerService;
+
+    public BootifulRestController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping("/csutomers")
+    Collection<Customer> get() {
+        return this.customerService.findAll();
+    }
 }
