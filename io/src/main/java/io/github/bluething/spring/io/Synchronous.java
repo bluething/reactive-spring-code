aabb824ea05a490fdb2fed60eecac627139435e1
bluething <habib.gunadarma@gmail.com>
2021-08-23 17:40:55 +0700

    Synchronous reader class

---
+++ io/src/main/java/io/github/bluething/spring/io/Synchronous.java
@@ -0,0 +1,16 @@
+package io.github.bluething.spring.io;
+
+import lombok.extern.log4j.Log4j2;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.function.Consumer;
+
+@Log4j2
+public class Synchronous implements Reader {
+    @Override
+    public void read(File file, Consumer<Bytes> consumer, Runnable finished) throws IOException {
+
+    }
+}
