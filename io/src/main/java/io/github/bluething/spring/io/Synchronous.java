390adde1d2ee707ca63878b75bb46eef36ab5e71
bluething <habib.gunadarma@gmail.com>
2021-08-23 17:44:58 +0700

    Read from the file
    Pull the result one line at the time

--- io/src/main/java/io/github/bluething/spring/io/Synchronous.java
+++ io/src/main/java/io/github/bluething/spring/io/Synchronous.java
@@ -1,6 +1,7 @@
 package io.github.bluething.spring.io;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.util.FileCopyUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -12,7 +13,12 @@ public class Synchronous implements Reader {
     @Override
     public void read(File file, Consumer<Bytes> consumer, Runnable finished) throws IOException {
         try (FileInputStream fin = new FileInputStream(file)) {
-
+            byte[] data = new byte[FileCopyUtils.BUFFER_SIZE];
+            int res;
+            while ((res = fin.read(data, 0, data.length)) != -1) {
+                consumer.accept(Bytes.from(data, res));
+            }
+            finished.run();
         }
     }
 }
