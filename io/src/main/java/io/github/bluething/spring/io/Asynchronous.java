560fdc18043fb6808b129a4bf8b2339ed3eab5ea
bluething <habib.gunadarma@gmail.com>
2021-08-24 14:23:55 +0700

    Using ExecutorService to invoke CompletionHandler when data is available

--- io/src/main/java/io/github/bluething/spring/io/Asynchronous.java
+++ io/src/main/java/io/github/bluething/spring/io/Asynchronous.java
@@ -1,18 +1,41 @@
 package io.github.bluething.spring.io;
 
+import lombok.extern.log4j.Log4j2;
+
 import java.io.File;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousFileChannel;
+import java.nio.channels.CompletionHandler;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Collections;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.function.Consumer;
 
-public class Asynchronous implements Reader {
+@Log4j2
+public class Asynchronous implements Reader, CompletionHandler<Integer, ByteBuffer> {
     private Consumer<Bytes> consumer;
     private Runnable finished;
+    private AsynchronousFileChannel fileChannel;
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
 
     @Override
     public void read(File file, Consumer<Bytes> consumer, Runnable finished) throws IOException {
         this.consumer = consumer;
         this.finished = finished;
         Path path = file.toPath();
+        this.fileChannel = AsynchronousFileChannel.open(path, Collections.singleton(StandardOpenOption.READ), this.executorService);
+    }
+
+    @Override
+    public void completed(Integer result, ByteBuffer attachment) {
+        
+    }
+
+    @Override
+    public void failed(Throwable exc, ByteBuffer attachment) {
+        log.error(exc);
     }
 }
