2464ef772369550e47b6cc3cacbf3446090c2448
bluething <habib.gunadarma@gmail.com>
2021-09-01 13:58:50 +0700

    Mono just
    Create a new Mono whose single value is a Date

--- reactor/src/test/java/io/github/bluething/spring/reactor/SimpleFluxFactoriesTest.java
+++ reactor/src/test/java/io/github/bluething/spring/reactor/SimpleFluxFactoriesTest.java
@@ -3,7 +3,10 @@ package io.github.bluething.spring.reactor;
 import org.junit.jupiter.api.Test;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
+
+import java.util.Date;
 
 public class SimpleFluxFactoriesTest {
     @Test
@@ -14,6 +17,10 @@ public class SimpleFluxFactoriesTest {
 
         Flux<String> letters = Flux.just("A", "B", "C");
         StepVerifier.create(letters).expectNext("A", "B", "C").verifyComplete();
+
+        long now = System.currentTimeMillis();
+        Mono<Date> greetingMono = Mono.just(new Date(now));
+        StepVerifier.create(greetingMono).expectNext(new Date(now)).verifyComplete();
 
     }
 }
