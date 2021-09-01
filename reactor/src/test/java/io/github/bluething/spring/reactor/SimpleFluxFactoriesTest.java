fe159f6db5d75df7764910e05ad1276b7c9f60f2
bluething <habib.gunadarma@gmail.com>
2021-09-01 14:09:18 +0700

    Flux fromStream
    Create new Flux from Java 8 Stream

--- reactor/src/test/java/io/github/bluething/spring/reactor/SimpleFluxFactoriesTest.java
+++ reactor/src/test/java/io/github/bluething/spring/reactor/SimpleFluxFactoriesTest.java
@@ -8,6 +8,9 @@ import reactor.test.StepVerifier;
 
 import java.util.Arrays;
 import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 public class SimpleFluxFactoriesTest {
     @Test
@@ -31,6 +34,11 @@ public class SimpleFluxFactoriesTest {
 
         Flux<Integer> fromIterable = Flux.fromIterable(Arrays.asList(1, 2, 3));
         StepVerifier.create(fromIterable).expectNext(1, 2, 3).verifyComplete();
+
+        AtomicInteger integer = new AtomicInteger();
+        Supplier<Integer> supplier = integer::incrementAndGet;
+        Flux<Integer> integerFlux = Flux.fromStream(Stream.generate(supplier));
+        StepVerifier.create(integerFlux.take(3)).expectNext(1).expectNext(2).expectNext(3).verifyComplete();
 
     }
 }
