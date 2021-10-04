b3c151e7ca99225214cf9169205f72c64287b062
bluething <habib.gunadarma@gmail.com>
2021-10-04 16:36:24 +0700

    Converting to and from Reactive Streams type

---
+++ reactor/src/test/java/io/github/bluething/spring/reactive/joshbook/reactor/FlowAndReactiveStreamsTest.java
@@ -0,0 +1,29 @@
+package io.github.bluething.spring.reactive.joshbook.reactor;
+
+import org.junit.jupiter.api.Test;
+import org.reactivestreams.FlowAdapters;
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.concurrent.Flow;
+
+public class FlowAndReactiveStreamsTest {
+
+    @Test
+    public void convert() {
+        Flux<Integer> original = Flux.range(0, 10);
+
+        Flow.Publisher<Integer> rangeOfIntegerAsJdk9Flow = FlowAdapters
+                .toFlowPublisher(original);
+        Publisher<Integer> rangeOfIntegerAsReactiveStream = FlowAdapters
+                .toPublisher(rangeOfIntegerAsJdk9Flow);
+
+        StepVerifier.create(original)
+                .expectNextCount(10)
+                .verifyComplete();
+        StepVerifier.create(rangeOfIntegerAsReactiveStream)
+                .expectNextCount(10)
+                .verifyComplete();
+    }
+}
