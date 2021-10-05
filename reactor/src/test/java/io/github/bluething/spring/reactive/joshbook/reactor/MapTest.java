05b843c20928cc4dd1b0397f2bb4382272e88d75
bluething <habib.gunadarma@gmail.com>
2021-10-05 16:37:08 +0700

    map operator

---
+++ reactor/src/test/java/io/github/bluething/spring/reactive/joshbook/reactor/MapTest.java
@@ -0,0 +1,17 @@
+package io.github.bluething.spring.reactive.joshbook.reactor;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+public class MapTest {
+    @Test
+    public void maps() {
+        var data = Flux.just("a", "b", "c")
+                .map(String::toUpperCase);
+
+        StepVerifier.create(data)
+                .expectNext("A", "B", "C")
+                .verifyComplete();
+    }
+}
